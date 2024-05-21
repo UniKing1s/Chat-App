7d13a870b8fba9b34a7aa0c4aa359fcaa1f731cf
UniKing1s <khoivo050603it@gmail.com>
2024-05-21 18:58:59 +0700

    update API

--- src/apiService/config.js
+++ src/apiService/config.js
@@ -1,5 +1,6 @@
 // import "dotenv/config";
 // export const USER_API = process.env.USER_API;
-export const host = "https://back-end-chat-app.onrender.com";
+// export const host = "https://back-end-chat-app.onrender.com";
+export const host = "https://back-end-chat-app-jn9n.onrender.com";
 export const USER_API = `${host}/user`;
 export const MESSAGE_API = `${host}/message`;
